fa4ffcc293f87177019e11fcfa7940c9e4eb7b0e
azwii33 <azweenaammi370@gmail.com>
2024-12-04 10:12:20 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const RunServer = require('./Database/connection');
 const cors = require('cors');
 const TodoRouter = require('./Routes/TodoRouter');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 
 //json: javascript object notation
